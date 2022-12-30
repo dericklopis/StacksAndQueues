add6c49064be2cee2614ef2fe73600fcdd76c15d
dericklopis <stanforever08@gmail.com>
2022-12-30 18:06:46 +0530

    Added: Dequeue

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -12,24 +12,19 @@ namespace StacksAndQueues
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stacks and Queues Problem\n");
-            stackAndQueue stack = new stackAndQueue();
+            //stackAndQueue stack = new stackAndQueue();
             Queue qu = new Queue();
             qu.Enqueue(56);
             qu.Enqueue(30);
             qu.Enqueue(70);
-            Console.WriteLine("\nThe Elements Are Present In The Queue");
+            Console.WriteLine();
+            qu.Dequeue();
+            qu.Display();
+            qu.Dequeue();
+            qu.Display();
+            qu.Dequeue();
             qu.Display();
             Console.ReadLine();
-            //stack.Peek();
-            //stack.Pop();
-            //stack.Display();
-            //stack.Peek();
-            //stack.Pop();
-            //stack.Display();
-            //stack.Peek();
-            //stack.Pop();
-            //Console.WriteLine();
-            //stack.Display();
         }
     }
 }
