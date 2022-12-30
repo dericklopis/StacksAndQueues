9e2e4f78b260fbf9d60b4832bd41c9c172617d53
dericklopis <stanforever08@gmail.com>
2022-12-30 17:49:00 +0530

    Added:Stacks and Queues

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stacks and Queues Problem\n");
+            stackAndQueue stack = new stackAndQueue();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            Console.WriteLine("\nThe Elements Are Present In The Stack");
+            stack.Display();
+            Console.ReadLine();
+        }
+    }
+}
